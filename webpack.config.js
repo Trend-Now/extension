28b2c33738c8a3757f1262bd4c88cf60c664d890
GuNyangE <noel7145@gmail.com>
2025-10-04 20:55:18 +0900

    아이콘 변경

--- webpack.config.js
+++ webpack.config.js
@@ -169,7 +169,7 @@ var options = {
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: 'src/assets/img/icon-128.png',
+          from: 'src/assets/img/tn120.png',
           to: path.join(__dirname, 'build'),
           force: true,
         },
@@ -178,7 +178,7 @@ var options = {
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: 'src/assets/img/icon-34.png',
+          from: 'src/assets/img/tn32.png',
           to: path.join(__dirname, 'build'),
           force: true,
         },
